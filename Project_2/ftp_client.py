917a15a1e2187fae68f21a2909136923b64fe1f3
jdaoud05 <jaddaoud05@gmail.com>
2026-01-24 21:48:59 -0500

    Tested server

--- Project_2/ftp_client.py
+++ Project_2/ftp_client.py
@@ -5,21 +5,24 @@ import socket
 def control_connect(HOST, PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
+    welcome = recv_reply(sock)  # ← Read welcome message!
+    print(f"Server: {welcome}")
+    return sock
 
 def pasv_connect(HOST, PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
     return sock
 
-def send_command(sock, command)
-    sock.send(command.encode() + "/r/n")
+def send_command(sock, command):
+    sock.send(command.encode() + b"\r\n")
     return recv_reply(sock)
 
-def login(sock, username, password)
+def login(sock, username, password):
     response = send_command(sock, f"USER {username}")
     print(f"Server: USER {username}")
-    if not password:
-        response = send_command(sock, f"PASS" {password})
+    if password:
+        response = send_command(sock, f"PASS {password}")
         print(f"Server: PASS {password}")
     return response
 
@@ -32,8 +35,14 @@ def recv_reply(sock):
         buffer += data  
     return buffer.decode(errors="ignore")
 
+def main():
+#    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serv = control_connect("ftp.4700.network", 21)
+    login(serv, 'daoud.ja', '97c30eb79ad37d6013afd7acee24226c49d705b1c4c40567449ae6532fd83cc3')
+    print("test")
 
-
+if __name__ == "__main__":
+    main()
 # def handle_ls():
 
 # def handle_mkdir():
